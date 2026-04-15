5665971ebc7efbd0a0c8453ace054bafe994cea8
Zineb Aourid <zineb.aourid1@gmail.com>
2026-04-14 22:56:01 -0400

    updated greedy

--- algorithms/greedy/activitySelection.py
+++ algorithms/greedy/activitySelection.py
@@ -9,25 +9,42 @@ Output: max # of activities, such that there is no overlapping activities.
 
 a = [(1,4), (3,5), (0,6), (5,7), (3,9), (5,9), (6,10), (8,11), (8,12), (2,14), (12,16)]
 
-def subsetting(L):
-    """
-    Given an integer array nums of unique elements, return all possible subsets (the power set).
+def is_valid(subset):
+    for i in range(len(subset)):
+        for j in range(i+1, len(subset)):
+            s1, f1 = subset[i]
+            s2, f2 = subset[j]
 
-    The solution set must not contain duplicate subsets. Return the solution in any order.
-    """
-    result = [[]]
-    for num in L:
-        result += [curr + [num] for curr in result]
-        #print(result)
-    return result
+            # TODO: check overlap
+            if f1 > s2 and f2 > s1:
+                return False
+    return True
 
-a = [(1,4)(3,5)(5,7)]
-print(subsetting(a))
 
-def activitySelec(L):
-    subset = subsetting(L)
-    maxCount = 0
-    for i in subset:
-        for j in i:
-            pass
-    return 0
+def generate_subsets(activities, index, current, result):
+    if index == len(activities):
+        result.append(current[:])
+        return
+
+    # include
+    current.append(activities[index])
+    generate_subsets(activities, index+1, current, result)
+
+    # exclude
+    current.pop()
+    generate_subsets(activities, index+1, current, result)
+
+
+def brute_force_activity(activities):
+    subsets = []
+    generate_subsets(activities, 0, [], subsets)
+
+    max_count = 0
+
+    for subset in subsets:
+        if is_valid(subset):
+            max_count = max(max_count, len(subset))
+
+    return max_count
+
+print(brute_force_activity(a))
